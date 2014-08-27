b7f5369d71ab530575c5e76047c5d3bdd2c515ec
J.F.J. Laros <j.f.j.laros@lumc.nl>
2014-08-27 20:41:30 +0200

    Some refactoring.

--- gene_disease_pubmed/gene_disease_pubmed.py
+++ gene_disease_pubmed/gene_disease_pubmed.py
@@ -8,46 +8,15 @@ import restkit
 
 from Bio import Entrez
 
-Entrez.email = "J.F.J.Laros@lumc.nl"
-
-hgnc_resource = "http://rest.genenames.org/search/"
-hgnc_query = "status:Approved"
-
-search_terms = [
-    "mental retardation",
-    "mental retardation profound",
-    "mental retardation range",
-    "mental retardation ranging",
-    "mental retardation syndrome",
-    "mentally retarded",
-    "learning disabilities",
-    "learning problems",
-    "mental deficiency",
-    "mental delay",
-    "mental detoriation",
-    "mental impairment",
-    "mental lethargy",
-    "mental regression",
-    "mental retardation congenital",
-    "mental retardation gene",
-    "development abnormalities",
-    "developmental delay",
-    "developmental disorder",
-    "developmental retardation",
-    "developmental stagnation",
-    "developmental decline",
-    "intellectual disability",
-    "intellectual impairment",
-    "intellectual regression",
-    "development deficit",
-    "development delayed",
-    "development delay",
-    "development failure",
-    "developmental stagnation"
-]
-
 def _get_key(record, keys):
     """
+    :arg record:
+    :type record:
+    :arg keys:
+    :type keys: tuple(str)
+
+    :returns:
+    :rtype:
     """
     if record.has_key(keys[0]):
         if len(keys) == 1:
@@ -57,15 +26,33 @@ def _get_key(record, keys):
     return None
 #_get_key
 
-def abstract_walker(query):
+def hgnc_genes(query="status:Approved"):
     """
-    """
-    batch_size = 100
-    search_results = Entrez.read(Entrez.esearch(db="pubmed",
-        term=query, reldate=730, datetype="pdat", usehistory="y"))
-    count = int(search_results["Count"])
+    :arg query:
+    :type query: str
 
-    for start in range(0, count, batch_size):
+    :returns:
+    :rtype:
+    """
+    hgnc_resource = "http://rest.genenames.org/search/"
+
+    resource = restkit.Resource(hgnc_resource)
+    response = resource.get(query, headers={"Accept": "application/json"})
+
+    return json.load(response.body_stream())
+#hgnc_genes
+
+def abstract_walker(search_results, batch_size=1000):
+    """
+    :arg search_results:
+    :type search_results:
+    :arg batch_size:
+    :type batch_size: int
+
+    :returns:
+    :rtype:
+    """
+    for start in range(0, int(search_results["Count"]), batch_size):
         fetch_handle = Entrez.efetch(db="pubmed", rettype="medline",
             retmode="xml", retstart=start, retmax=batch_size,
             webenv=search_results["WebEnv"],
@@ -78,61 +65,88 @@ def abstract_walker(query):
                 title = _get_key(body, ('ArticleTitle', ))
                 text = _get_key(body, ('Abstract', 'AbstractText'))
                 if text:
-                    text = text[0]
+                    yield title, text[0]
                 else:
-                    text = ""
-
-                yield title, text
+                    yield title, ""
             #if
         #for
         fetch_handle.close()
     #for
 #abstract_walker
 
-def hgnc_genes():
+def search_abstracts(search_terms, reldate=730):
     """
+    :arg search_terms:
+    :type search_terms: list(str)
+    :arg reldate:
+    :type reldate: int
+
+    :returns:
+    :rtype:
     """
-    resource = restkit.Resource(hgnc_resource)
-    response = resource.get(hgnc_query, headers={"Accept": "application/json"})
-    records = json.load(response.body_stream())
-
-    return map(lambda record: record['symbol'], records['response']['docs'])
-#hgnc_genes
-
-def gene_disease_pubmed(handle):
-    """
-    """
-    hits = collections.defaultdict(int)
-
-    sys.stdout.write("Retrieving gene list.\n")
-    genes = hgnc_genes()
-
-    sys.stdout.write("Starting analysis.\n")
     query = " OR ".join(search_terms)
-    walker = abstract_walker(query)
+
+    return Entrez.read(Entrez.esearch(db="pubmed", term=query, reldate=reldate,
+        datetype="pdat", usehistory="y"))
+#search_abstracts
+
+def gene_disease_pubmed(input_handle, output_handle, log_handle, email,
+        progress_indicator=100):
+    """
+    :arg input_handle:
+    :type input_handle: handle
+    :arg output_handle:
+    :type output_handle: handle
+    :arg log_handle:
+    :type log_handle: handle
+    :arg email:
+    :type email: str
+    :arg progress_indicator:
+    :type progress_indicator: int
+    """
+    Entrez.email = email
+
+    log_handle.write("Retrieving gene list ... ")
+    log_handle.flush()
+    genes = set(map(lambda x: x['symbol'], hgnc_genes()['response']['docs']))
+    log_handle.write("found {}.\n".format(len(genes)))
+
+    log_handle.write("Searching abstracts ... ")
+    log_handle.flush()
+    search_terms = map(lambda x: x.strip(), input_handle.readlines())
+    search_results = search_abstracts(search_terms)
+    log_handle.write("found {}.\n".format(search_results["Count"]))
+
+    walker = abstract_walker(search_results)
+    log_handle.write("Starting analysis ({} abstracts per dot) ".format(
+        progress_indicator))
+    log_handle.flush()
+
     indicator = 0
+    hits = collections.defaultdict(int)
     for abstract in walker:
-        if not indicator % 100:
-            sys.stdout.write(".")
-            sys.stdout.flush()
+        if not indicator % progress_indicator:
+            log_handle.write(".")
+            log_handle.flush()
         #if
         indicator += 1
         title = abstract[0].split()
         text = abstract[1].split()
+        total_text = set(title + text)
 
-        for gene in genes:
-            if gene in title + text:
-                hits[gene] += 1
+        for gene in genes & total_text:
+            hits[gene] += 1
     #for
 
     for hit in hits:
-        handle.write("{}\t{}\n".format(hit, hits[hit]))
+        output_handle.write("{}\t{}\n".format(hit, hits[hit]))
 #gene_disease_pubmed
 
 def main():
     """
     """
-    gene_disease_pubmed(open("results.txt", "w"))
+    gene_disease_pubmed(open("query.txt"), open("results.txt", "w"),
+        sys.stdout, "J.F.J.Laros@lumc.nl")
 #main
 
 if __name__ == "__main__":
