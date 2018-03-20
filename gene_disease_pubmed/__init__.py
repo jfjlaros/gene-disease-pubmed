e06d4b973131932769a91a48c4dd864b3c90b77b
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-03-20 11:46:45 +0100

    Pep-8.

--- gene_disease_pubmed/__init__.py
+++ gene_disease_pubmed/__init__.py
@@ -1,24 +1,23 @@
-"""
-PubMed abstract query post processor.
+"""PubMed abstract query post processor.
 
 
-Copyright (c) 2014 Leiden University Medical Center <humgen@lumc.nl>
-Copyright (c) 2014 Jeroen F.J. Laros <j.f.j.laros@lumc.nl>
+Copyright (c) 2014 Jeroen F.J. Laros
 
 Licensed under the MIT license, see the LICENSE file.
 """
-
 import argparse
 import os
+
 
 __version_info__ = ('0', '0', '1')
 
 __version__ = '.'.join(__version_info__)
-__author__ = 'LUMC, Jeroen F.J. Laros'
+__author__ = 'Jeroen F.J. Laros'
 __contact__ = 'J.F.J.Laros@lumc.nl'
 __homepage__ = 'https://git.lumc.nl/j.f.j.laros/gene-disease-pubmed/tree/master'
 
-usage = __doc__.split("\n\n\n")
+usage = __doc__.split('\n\n\n')
+
 
 class ProtectedFileType(argparse.FileType):
     def __call__(self, string):
@@ -26,9 +25,11 @@ class ProtectedFileType(argparse.FileType):
             raise IOError('failed to create "{}": file exists.'.format(string))
         return super(ProtectedFileType, self).__call__(string)
 
+
 def doc_split(func):
-    return func.__doc__.split("\n\n")[0]
+    return func.__doc__.split('\n\n')[0]
+
 
 def version(name):
-    return "%s version %s\n\nAuthor   : %s <%s>\nHomepage : %s" % (name,
-        __version__, __author__, __contact__, __homepage__)
+    return '{} version {}\n\nAuthor   : {} <{}>\nHomepage : {}'.format(
+        name, __version__, __author__, __contact__, __homepage__)
--- gene_disease_pubmed/gene_disease_pubmed.py
+++ gene_disease_pubmed/gene_disease_pubmed.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-
 import argparse
 import collections
 import json
@@ -12,56 +10,50 @@ from Bio import Entrez
 
 from . import ProtectedFileType, usage, version
 
+
 def _get_key(record, keys):
     """
-    :arg record:
-    :type record:
-    :arg keys:
-    :type keys: tuple(str)
+    :arg object record:
+    :arg tuple keys:
 
-    :returns:
-    :rtype:
+    :returns object:
     """
     if record.has_key(keys[0]):
         if len(keys) == 1:
             return record[keys[0]]
         return _get_key(record[keys[0]], keys[1:])
-    #if
     return None
-#_get_key
 
-def hgnc_genes(query="status:Approved"):
-    """
-    :arg query:
-    :type query: str
 
-    :returns:
-    :rtype:
+def hgnc_genes(query='status:Approved'):
     """
-    hgnc_resource = "http://rest.genenames.org/search/"
+    :arg str query:
+
+    :returns object:
+    """
+    hgnc_resource = 'http://rest.genenames.org/search/'
 
     resource = restkit.Resource(hgnc_resource)
-    response = resource.get(query, headers={"Accept": "application/json"})
+    response = resource.get(query, headers={'Accept': 'application/json'})
 
     return json.load(response.body_stream())
-#hgnc_genes
+
 
 def abstract_walker(search_results, batch_size=1000):
     """
-    :arg search_results:
-    :type search_results:
-    :arg batch_size:
-    :type batch_size: int
+    :arg object search_results:
+    :arg int batch_size:
 
-    :returns:
-    :rtype:
+    :returns object:
     """
-    for start in range(0, int(search_results["Count"]), batch_size):
-        fetch_handle = Entrez.efetch(db="pubmed", rettype="medline",
-            retmode="xml", retstart=start, retmax=batch_size,
-            webenv=search_results["WebEnv"],
-            query_key=search_results["QueryKey"])
-        records = Entrez.parse(fetch_handle)
+    for start in range(0, int(search_results['Count']), batch_size):
+        fetch_handle = Entrez.efetch(
+            db='pubmed', rettype='medline', retmode='xml', retstart=start,
+            retmax=batch_size, webenv=search_results['WebEnv'],
+            query_key=search_results['QueryKey'])
+        records = Entrez.read(fetch_handle)['PubmedArticle']
+        fetch_handle.close()
+
         for record in records:
             body = _get_key(record, ('MedlineCitation', 'Article'))
 
@@ -69,7 +61,7 @@ def abstract_walker(search_results, batch_size=1000):
                 title = _get_key(body, ('ArticleTitle', ))
                 text = _get_key(body, ('Abstract', 'AbstractText'))
 
-                pmid = "0"
+                pmid = '0'
                 for i in _get_key(record, ('PubmedData', 'ArticleIdList')):
                     if i.attributes['IdType'] == 'pubmed':
                         pmid = str(i)
@@ -77,61 +69,50 @@ def abstract_walker(search_results, batch_size=1000):
                 if text:
                     yield title, text[0], pmid
                 else:
-                    yield title, "", pmid
-            #if
-        #for
-        fetch_handle.close()
-    #for
-#abstract_walker
+                    yield title, '', pmid
+
 
 def search_abstracts(search_terms, reldate):
     """
-    :arg search_terms:
-    :type search_terms: list(str)
-    :arg reldate:
-    :type reldate: int
+    :arg list search_terms:
+    :arg int reldate:
 
-    :returns:
-    :rtype:
+    :returns object:
     """
-    query = "\"{}\"".format("\" OR \"".join(search_terms))
+    query = '"{}"'.format('" OR "'.join(search_terms))
 
-    return Entrez.read(Entrez.esearch(db="pubmed", term=query, reldate=reldate,
-        datetype="pdat", usehistory="y"))
-#search_abstracts
+    return Entrez.read(Entrez.esearch(
+        db='pubmed', term=query, reldate=reldate, datetype='pdat',
+        usehistory='y'))
 
-def gene_disease_pubmed(input_handle, output_handle, log_handle, email,
-        reldate=730, progress_indicator=100):
+
+def gene_disease_pubmed(
+        input_handle, output_handle, log_handle, email, reldate=730,
+        progress_indicator=100):
     """
-    :arg input_handle:
-    :type input_handle: handle
-    :arg output_handle:
-    :type output_handle: handle
-    :arg log_handle:
-    :type log_handle: handle
-    :arg email:
-    :type email: str
-    :arg reldate:
-    :type reldate: int
-    :arg progress_indicator:
-    :type progress_indicator: int
+    :arg stream input_handle:
+    :arg stream output_handle:
+    :arg stream log_handle:
+    :arg str email:
+    :arg int reldate:
+    :arg int progress_indicator:
     """
     Entrez.email = email
-    trim_pattern = "[\W_]+"
+    trim_pattern = '[\W_]+'
 
-    log_handle.write("Retrieving gene list ... ")
+    log_handle.write('Retrieving gene list ... ')
     log_handle.flush()
     genes = set(map(lambda x: x['symbol'], hgnc_genes()['response']['docs']))
-    log_handle.write("found {}.\n".format(len(genes)))
+    log_handle.write('found {}.\n'.format(len(genes)))
 
-    log_handle.write("Searching abstracts ... ")
+    log_handle.write('Searching abstracts ... ')
     log_handle.flush()
     search_terms = map(lambda x: x.strip(), input_handle.readlines())
     search_results = search_abstracts(search_terms, reldate)
-    log_handle.write("found {}.\n".format(search_results["Count"]))
+    log_handle.write('found {}.\n'.format(search_results['Count']))
 
     walker = abstract_walker(search_results)
-    log_handle.write("Starting analysis ({} abstracts per dot) ".format(
+    log_handle.write('Starting analysis ({} abstracts per dot) '.format(
         progress_indicator))
     log_handle.flush()
 
@@ -141,43 +122,45 @@ def gene_disease_pubmed(input_handle, output_handle, log_handle, email,
     trim_suffix = re.compile('{}$'.format(trim_pattern))
     for abstract in walker:
         if not indicator % progress_indicator:
-            log_handle.write(".")
+            log_handle.write('.')
             log_handle.flush()
-        #if
         indicator += 1
         title = abstract[0].split()
         text = abstract[1].split()
         pmid = abstract[2]
-        total_text = set(map(lambda x: trim_prefix.sub('', trim_suffix.sub('',
-            x)), set(title + text)))
+        total_text = set(map(lambda x: trim_prefix.sub(
+            '', trim_suffix.sub('', x)), set(title + text)))
 
         for gene in genes & total_text:
             hits[gene][0] += 1
             hits[gene][1].append(pmid)
-    #for
 
     for hit in hits:
-        output_handle.write("{}\t{}\t{}\n".format(hit, hits[hit][0],
-            ' '.join(hits[hit][1])))
-    log_handle.write(" done.\n")
-#gene_disease_pubmed
+        output_handle.write('{}\t{}\t{}\n'.format(
+            hit, hits[hit][0], ' '.join(hits[hit][1])))
+    log_handle.write(' done.\n')
+
 
 def main():
-    """
-    """
+    """Main entry point."""
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=usage[0], epilog=usage[1])
-    parser.add_argument('-v', action="version", version=version(parser.prog))
-    parser.add_argument('input_handle', metavar='INPUT',
-        type=argparse.FileType('r'), help='file containing a list of queries')
-    parser.add_argument('output_handle', metavar='OUTPUT',
-        type=ProtectedFileType('w'), help='output file')
-    parser.add_argument('email', metavar='EMAIL',
-        type=str, help='email address (%(type)s)')
-    parser.add_argument('-o', dest='log_handle', default=sys.stdout,
+
+    parser.add_argument('-v', action='version', version=version(parser.prog))
+    parser.add_argument(
+        'input_handle', metavar='INPUT', type=argparse.FileType('r'),
+        help='file containing a list of queries')
+    parser.add_argument(
+        'output_handle', metavar='OUTPUT', type=ProtectedFileType('w'),
+        help='output file')
+    parser.add_argument(
+        'email', metavar='EMAIL', type=str, help='email address (%(type)s)')
+    parser.add_argument(
+        '-o', dest='log_handle', default=sys.stdout,
         type=argparse.FileType('w'), help='log file (default=<stdout>)')
-    parser.add_argument('-d', dest='reldate', type=int, default=730,
+    parser.add_argument(
+        '-d', dest='reldate', type=int, default=730,
         help='history window in days (%(type)s default=%(default)s)')
 
     try:
@@ -186,12 +169,12 @@ def main():
         parser.error(error)
 
     try:
-        gene_disease_pubmed(arguments.input_handle, arguments.output_handle,
-            arguments.log_handle, arguments.email,
-            arguments.reldate)
+        gene_disease_pubmed(
+            arguments.input_handle, arguments.output_handle,
+            arguments.log_handle, arguments.email, arguments.reldate)
     except ValueError, error:
         parser.error(error)
-#main
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     main()
